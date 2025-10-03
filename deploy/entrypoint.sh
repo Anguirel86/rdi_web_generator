0cb71ea6707def803432205d92f5ac22019b42d8
Anguirel <anguirel86@gmail.com>
2025-10-02 20:55:04 -0400

    Run form autogen on container entrypoint

--- deploy/entrypoint.sh
+++ deploy/entrypoint.sh
@@ -1,5 +1,23 @@
 #!/bin/bash
 
+# Autogenerate the toml generator html and python form code
+python tools/create_toml_gen_form.py
+
+if [[ $? -ne 0 ]]; then
+    echo "Failed to generate toml_gen form"
+    exit 1
+fi
+
+# Copy over the auto-generated html files
+AUTOGEN_HTML_PATH=generator/templates/generator/toml_gen
+mkdir -p $AUTOGEN_HTML_PATH
+cp form_gen_output/html/* $AUTOGEN_HTML_PATH
+
+# Copy over the auto-generated python form file
+cp form_gen_output/toml_gen_form.py generator/
+
+rm -r form_gen_output
+
 # Handle db migrations and static files on container startup
 python manage.py migrate
 python manage.py collectstatic --no-input --clear
